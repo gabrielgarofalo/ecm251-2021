de52fa1b58929af517aaff5125f898fd3ec6c700
gabrielgarofalo <73725151+gabrielgarofalo@users.noreply.github.com>
2021-03-30 17:52:24 -0300

    Atualização na classe Usuários
    Começo da criação da classe Usuario

--- AtividadeT1/src/garofalo/domingues/gabriel/Usuarios.java
+++ AtividadeT1/src/garofalo/domingues/gabriel/Usuarios.java
@@ -1,4 +1,13 @@
 package garofalo.domingues.gabriel;
 
 public class Usuarios {
+   private String nome;
+   private String senha;
+   private String email;
+
+   public Usuarios(String nome, String senha, String email){
+       this.nome = nome;
+       this.senha = senha;
+       this.email = email;
+   }
 }
