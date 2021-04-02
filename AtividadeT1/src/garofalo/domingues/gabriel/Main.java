154857716f67d3b8c19d9f077bf91679da19f6ce
gabrielgarofalo <73725151+gabrielgarofalo@users.noreply.github.com>
2021-04-02 00:20:07 -0300

    Comentando o código
    Adicão de comentários

--- AtividadeT1/src/garofalo/domingues/gabriel/Main.java
+++ AtividadeT1/src/garofalo/domingues/gabriel/Main.java
@@ -3,14 +3,17 @@ package garofalo.domingues.gabriel;
 public class Main {
 
     public static void main(String[] args) {
+        // Criação dos usuários
         Usuarios user1 = new Usuarios("All Might");
         Usuarios user2 = new Usuarios("One For All");
         Usuarios user3 = new Usuarios("Bakugo");
 
+        // Criação das contas dos usuários
         Contas c1 = new Contas(user1, 1000);
         Contas c2 = new Contas(user2, 250);
         Contas c3 = new Contas(user3, 3000);
 
+        // Estado Inicial das Contas
         System.out.println("Estado Inical:");
         System.out.println("Conta 1:" + c1.toString());
         System.out.println("Conta 2:" + c2.toString());
@@ -18,16 +21,17 @@ public class Main {
 
         Transacoes transacao = new Transacoes();
 
-        String op1 = transacao.criarQRCode(c1,250);
+        String op1 = transacao.criarQRCode(c1,250); // Usuário 1 gera uma operação de recebimento de 250
 
-        transacao.pagamento(c2, c1, op1);
-        transacao.pagamento(c3, c1, op1);
-        transacao.pagamento(c2, c1, op1);
+        transacao.pagamento(c2, c1, op1); // Usário 2 paga a operação
+        transacao.pagamento(c3, c1, op1); // Usário 3 paga a operação
+        transacao.pagamento(c2, c1, op1); // Usário 2 paga a operação novamente
 
-        String op2 = transacao.criarQRCode(c2,1000);
+        String op2 = transacao.criarQRCode(c2,1000); // Usuário 2 gera uma operação de recebimento de 1000
 
-        transacao.pagamento(c3, c2, op2);
+        transacao.pagamento(c3, c2, op2); // Usário 3 paga a operação
 
+        // Estado Final das Contas
         System.out.println("Estado Final:");
         System.out.println("Conta 1:" + c1.toString());
         System.out.println("Conta 2:" + c2.toString());
--- AtividadeT1/src/garofalo/domingues/gabriel/Transacoes.java
+++ AtividadeT1/src/garofalo/domingues/gabriel/Transacoes.java
@@ -4,19 +4,19 @@ import java.util.Random;
 
 public class Transacoes {
 
-    private static int getRandomNumberInRange ( int min , int max ) {
+    private static int getRandomNumberInRange ( int min , int max ) { // Função para gerar número aleátorio entre 2 int
         Random r = new Random ();
         return r . nextInt (( max - min ) + 1 ) + min ;
     }
 
-    public String criarQRCode(Contas conta, double valor){
+    public String criarQRCode(Contas conta, double valor){ // Função para criar o QRCode com os dados especificados
         return conta.getIdConta() + ";" + conta.getUsuario().getNome() + ";" + valor + ";" + getRandomNumberInRange(1000,9999);
     }
 
     public boolean pagamento(Contas pagador, Contas recebedor, String s){
-        String [] dados = s.split(";");
-        if((recebedor.getUsuario().getNome().equals(dados[1])) && (Integer.toString(recebedor.getIdConta()).equals(dados[0])) && (pagador.getSaldo() >= Double.parseDouble(dados[2]))){
-            pagador.transferirDinheiro(recebedor, Double.parseDouble(dados[2]));
+        String [] dados = s.split(";"); // Dividir string em uma lista com cada campo um dado
+        if((recebedor.getUsuario().getNome().equals(dados[1])) && (Integer.toString(recebedor.getIdConta()).equals(dados[0])) && (pagador.getSaldo() >= Double.parseDouble(dados[2]))){ // Verificação de nome, id e saldo
+            pagador.transferirDinheiro(recebedor, Double.parseDouble(dados[2])); // Trnsferência entre as contas
             //System.out.println("Operação Válida!");
             return true;
         } else{
