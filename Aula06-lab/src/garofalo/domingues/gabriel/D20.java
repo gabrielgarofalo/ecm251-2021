d7df34dd5436fe0176060b7ebb47e4a241fee709
gabrielgarofalo <73725151+gabrielgarofalo@users.noreply.github.com>
2021-04-05 09:07:54 -0300

    Último código

---
+++ Aula06-lab/src/garofalo/domingues/gabriel/D20.java
@@ -0,0 +1,11 @@
+package garofalo.domingues.gabriel;
+
+public class D20 {
+    public class D20 extends Dado{
+        final public static int TOTAL_DE_FACES = 20;
+        public D20(){
+            super(TOTAL_DE_FACES);
+        }
+    }
+
+}
---
+++ Aula06-lab/src/garofalo/domingues/gabriel/D6.java
@@ -0,0 +1,11 @@
+package garofalo.domingues.gabriel;
+
+public class D6 {
+    public class D6 extends Dado{
+        final public static int TOTAL_DE_FACES = 6;
+        public D6(){
+            super(TOTAL_DE_FACES);
+        }
+    }
+
+}
