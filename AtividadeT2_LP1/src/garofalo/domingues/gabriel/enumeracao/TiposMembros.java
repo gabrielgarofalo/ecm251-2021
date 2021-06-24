a2683b43f54dde9a07453035fe7ff501e6d74587
gabrielgarofalo <73725151+gabrielgarofalo@users.noreply.github.com>
2021-06-24 15:13:51 -0300

    Criação da enum TipoMembros e Modelo de um Membro

---
+++ AtividadeT2_LP1/src/garofalo/domingues/gabriel/enumeracao/TiposMembros.java
@@ -0,0 +1,5 @@
+package garofalo.domingues.gabriel.enumeracao;
+
+public enum TiposMembros {
+    Mobile_Members, Heavy_Lifters, Script_Guys, Big_Brothers;
+}
---
+++ AtividadeT2_LP1/src/garofalo/domingues/gabriel/models/Membro.java
@@ -0,0 +1,22 @@
+package garofalo.domingues.gabriel.models;
+
+public class Membro {
+    String nome_usuario;
+    String email;
+    String funcao;
+
+    public Membro(String nome_usuario, String email, String funcao) {
+        this.nome_usuario = nome_usuario;
+        this.email = email;
+        this.funcao = funcao;
+    }
+
+    @Override
+    public String toString() {
+        return "Membro{" +
+                "nome_usuario='" + nome_usuario + '\'' +
+                ", email='" + email + '\'' +
+                ", funcao='" + funcao + '\'' +
+                '}';
+    }
+}
